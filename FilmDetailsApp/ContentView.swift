15e048240f618387943593213fec35040addb37f
muhammedfaruk <76944306+muhammedfaruk@users.noreply.github.com>
2021-10-09 14:29:36 +0300

    Initial Commit

---
+++ FilmDetailsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FilmDetailsApp
+//
+//  Created by Muhammed Faruk Söğüt on 9.10.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FilmDetailsApp/FilmDetailsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmDetailsAppApp.swift
+//  FilmDetailsApp
+//
+//  Created by Muhammed Faruk Söğüt on 9.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct FilmDetailsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
